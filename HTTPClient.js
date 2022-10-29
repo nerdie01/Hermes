5e0ae97c993db401afbfa91ee4c558a34339ba5f
nerdie01 <varuniyengar01@gmail.com>
2022-10-29 19:13:59 -0400

    Fix networking

--- HTTPClient.js
+++ HTTPClient.js
@@ -1,25 +1,33 @@
 import * as React from 'react';
 
-export var ws = new WebSocket('ws://192.168.86.28:25565');
+export var ws = new WebSocket('ws://192.168.86.28:25565');;
 export var summary, transcription, emotions;
 
-export function sendToClient(msg) {
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export async function sendToClient(msg) {
     try {
-        //send a start token to the server
         ws.send('START');
-        //divide msg into 1000 char chunks and send
-        var chunkSize = 10000;
+        console.log('Sent START token to server');
+
+        var chunkSize = 1000;
         var chunks = [];
         for (var i = 0; i < msg.length; i += chunkSize) {
             chunks.push(msg.substring(i, i + chunkSize));
         }
+
         for (var i = 0; i < chunks.length; i++) {
             ws.send(chunks[i]);
+            setTimeout(() => {}, 1000);
         }
+
         ws.send('END');
+        console.log('Sent END token to server');
     }
-    catch (err) {
-        console.log(err);
+    catch {
+        console.error('Error sending message to server: ', error);
     }
 }
 
